1fe5fd98b7d31b506f86ee121414764fd2a3d5c7
DiegoDimap <diegodimap@gmail.com>
2026-05-14 08:24:48 -0300

    teste falhando para verificar o pipeline

--- src/test/java/br/org/edu/ifrn/LojaCarro/services/CarroServiceTest.java
+++ src/test/java/br/org/edu/ifrn/LojaCarro/services/CarroServiceTest.java
@@ -60,7 +60,7 @@ class CarroServiceTest {
 
         carroService.deleteById(id);
 
-        verify(carroRepository, times(1)).deleteById(id);
+        verify(carroRepository, times(10)).deleteById(id);
         verifyNoMoreInteractions(carroRepository);
     }
 
